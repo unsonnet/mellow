eaee7c44b8233d73ce31821c22294a122aa904e2
QubicLens <srodvasquez@gmail.com>
2019-11-29 23:10:02 -0500

    Update network class docs

--- mellow/core.py
+++ mellow/core.py
@@ -5,17 +5,34 @@ import warnings
 import jax.numpy as np
 import jax.ops as jo
 
+from mellow.typing import Array, Tensor, UFunc
 from mellow import nn
 
 
 class Network(object):
     """Homogenous feedforward neural network."""
 
-    def __init__(self, inp, out, params, act):
+    def __init__(self, inp: int, out: int, params: Array, act: UFunc) -> None:
+        """Inits Network.
+
+        Deduces the structure of a network represented by `inp`, `out`,
+        and `params`. A weighted adjacency matrix and node vector of
+        appropriate shape are constructed as well.
+
+        Args:
+            inp: Number of input nodes excluding bias unit.
+            out: Number of output nodes.
+            params: One-dimensional array of weights.
+            act: Activation function.
+
+        Raises:
+            ValueError: If a network structure cannot be found with the
+                same number of arcs as weights in `params`.
+        """
         inb = inp + 1  # Accounts for bias unit.
         hid = nn.depth(inb, out, params)
 
-        with warnings.catch_warnings():
+        with warnings.catch_warnings():  # Filters precision warnings.
             warnings.filterwarnings("ignore", message="Explicitly requested dtype.*")
 
             self.shape = np.array([inb, hid, out], dtype=int)
@@ -24,14 +41,46 @@ class Network(object):
 
         self.A = act
 
-    def predict(self, z):
-        """Produces a hypothesis."""
+    def predict(self, z: Tensor) -> Tensor:
+        """Produces a hypothesis from `z`.
+        
+        Args:
+            z: Single or vertically-stacked array of input.
+
+        Returns:
+            Vertically-stacked output layers.
+
+        Raises:
+            AttributeError: If insufficient input data is given.
+        """
         return self.eval(self.θ, z)
 
-    def eval(self, θ, z):
-        """Evaluates network on input."""
+    def eval(self, θ: Tensor, z: Tensor) -> Tensor:
+        """Evaluates network on input.
+
+        Forward propagates data from `z` through network with arcs
+        parameterized by `θ`.
+        
+        Args:
+            θ: Topologically-sorted weighted adjacency matrix.
+            z: Single or vertically-stacked array of input.
+
+        Returns:
+            Vertically-stacked output layers.
+
+        Raises:
+            AttributeError: If insufficient input data is given.
+            ValueError: If network cannot be parameterized by `θ`.
+        """
         inb, _, out = self.shape
         v = self.reshape(z)
+
+        if self.θ.shape != θ.shape:
+            msg = (
+                "Weighted adjacency matrix of shape {} required to parameterize a {} "
+                "network, got {}."
+            )
+            raise ValueError(msg.format(self.θ.shape, self.shape, θ.shape))
 
         for idx in range(inb, v.shape[-1]):
             Σ = np.dot(v[..., :idx], θ[:idx, idx - inb])
@@ -39,12 +88,32 @@ class Network(object):
 
         return np.dot(v, θ[:, -out:])
 
-    def reshape(self, z):
-        """Formats data for network evaluation."""
-        inb, _, _ = self.shape
-        assert z.transpose().shape[0] == inb - 1
+    def reshape(self, z: Tensor) -> Tensor:
+        """Formats data for network evaluation.
 
-        rows = z.transpose()[..., None].shape[1]
+        Assigns data from `z` to input layer. Vectors are stacked
+        vertically if `z` represents multiple data samples.
+
+        Args:
+            z: Single or vertically-stacked array of input.
+
+        Returns:
+            Vertically-stacked node vectors.
+
+        Raises:
+            AttributeError: If input layers cannot be completely
+                initialized.
+        """
+        inb, _, _ = self.shape
+
+        if inb - 1 != np.transpose(z).shape[0]:
+            msg = (
+                "{} values required per input slice in order for network to evaluate "
+                "data, got {}."
+            )
+            raise AttributeError(msg.format(inb - 1, np.transpose(z).shape[0]))
+
+        rows = np.transpose(z)[..., None].shape[1]  # Counts number of data samples.
         v = np.tile(self.v, (rows, 1))
 
         return jo.index_update(v, jo.index[..., 1:inb], z)
--- mellow/typing.py
+++ mellow/typing.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from typing import Sequence, Union
+from typing import Sequence, Union, Callable
 
 import jax.numpy as np
 
@@ -8,3 +8,4 @@ import jax.numpy as np
 Shape = Sequence[int]
 Array = Sequence[float]
 Tensor = Union[float, np.ndarray]
+UFunc = Callable[[Tensor], Tensor]
